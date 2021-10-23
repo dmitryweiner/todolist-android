1600573d018947559914e624289e581c2c5aabfc
dmitryweiner <dmitry.weiner@gmail.com>
2021-10-23 12:41:07 +0700

    Save and restore list

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,12 +17,13 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private final ArrayList<String> list = new ArrayList<String>();
+    private ArrayList<String> list = new ArrayList<String>();
     private ArrayAdapter arrayAdapter;
 
     @Override
@@ -37,6 +38,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        outState.putSerializable("list", list);
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        list.addAll((ArrayList<String>) savedInstanceState.getSerializable("list"));
+        arrayAdapter.notifyDataSetChanged();
     }
 
     @Override
